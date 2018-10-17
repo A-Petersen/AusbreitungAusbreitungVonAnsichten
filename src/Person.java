1b52b5678a2edb678e38ab3258f6703aeb9c4618
A-Petersen <a.petersen@stud.fh-luebeck.de>
2018-10-17 10:40:23 +0200

    .

--- src/Person.java
+++ src/Person.java
@@ -2,11 +2,34 @@ import java.util.Random;
 
 public class Person {
 
-    private boolean AnsichtA;
-    private Random randGen;
+    private static int pIDCount;
+    private int pID;
 
-    Person (boolean m) {
+    private boolean AnsichtA;
+    /**
+     * Anzahl der vergangenen Treffen
+     */
+    private int anzTreffen;
+    /**
+     * Benötigte Anzahl von Treffen für abhängige Meinungsbildung
+     */
+    private int anzBenoetigterTreffen;
+    /**
+     * RandomGenerator
+     */
+    private Random randGen = new Random();
+
+    /**
+     * Konstruktor für Person
+     * @param m     initiale Ansicht A
+     * @param aT    Benötigte Anzahl von Treffen für abhängige Meinungsbildung
+     */
+    Person (boolean m, int aT)
+    {
+        pID = pIDCount;
         AnsichtA = m;
+        anzBenoetigterTreffen = aT;
+        pIDCount++;
     }
 
     /**
@@ -14,17 +37,40 @@ public class Person {
      * Wobei pA der eintretenden Wahrscheinlichkeit in Prozent entspricht, dass die Meinung
      * A gebildet wird.
      * Kann die Meinung nur setzen, nicht rücksetzen!!!
-     * @param pA Wahrscheinlichkeint der Meinungsbildung (int von 0... 100)
+     * @param pA    Wahrscheinlichkeint der Meinungsbildung (int von 0... 100)
      */
-    void unabhaengigeMeinung (int pA) {
-        int randomNum = randGen.nextInt(100);
-        this.AnsichtA = ( randomNum > pA && AnsichtA != true ) ? false : true;
+    void unabhaengigeMeinung (int pA)
+    {
+        int randomNum = randGen.nextInt(101);
+        AnsichtA = ( randomNum <= pA && AnsichtA != true ) ? false : true;
+    }
+
+    /**
+     * Triggert die abhängige Meinungsbildung A.
+     * Erhöht die Anzahl der Treffen um 1 und die Ansicht A auf true, sofern die
+     * nötige Anzahl von Treffen erreicht oder überschritten wurde
+     * und die getroffene Person die nötige Ansicht A vertritt.
+     */
+    void abhaengigeMeinung(Person p)
+    {
+        if (p.getAnsichtA()) {
+            if (anzTreffen >= anzBenoetigterTreffen) {
+                AnsichtA = true;
+            }
+            anzTreffen++;
+        }
     }
 
     /**
      * Setzt die Meinung/Ansicht A auf true.
      */
-    void abhaengigeMeinung () {
-        this.AnsichtA = true;
+    void setMeinung ()
+    {
+        AnsichtA = true;
+    }
+
+    boolean getAnsichtA()
+    {
+        return AnsichtA;
     }
 }
--- src/Tagesablauf.java
+++ src/Tagesablauf.java
@@ -1,3 +1,122 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 public class Tagesablauf {
 
+    /**
+     * Liste der Personen im Tag
+     */
+    private List<Person> personen;
+    /**
+     * RandomGenerator
+     */
+    private Random randGen = new Random();
+    private int anzMeinungA;
+
+    /**
+     * Konstruktor der Klasse Tagesablauf
+     * @param p Anzahl an Personen im Tag
+     * @param anzA Anzahl an Personen mit Meinung A
+     */
+    Tagesablauf (int p, int anzA)
+    {
+        personen = new LinkedList<Person>();
+        fuellePersonen(p, anzA);
+        anzMeinungA = anzA;
+    }
+
+    /**
+     * Füllt die Lsite Peronen, sofern sie noch nicht existiert und
+     * setzt die gewünschte Anzahl an Personen mit Meinung/Ansicht A.
+     * @param n Anzahl an Personen
+     * @param anzA Anzahl an Personen mit Meinung A
+     */
+    private void fuellePersonen (int n, int anzA)
+    {
+        if (!personen.isEmpty())
+        {
+            System.out.println("Personen Empty!");
+            return;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            Person p = new Person(false, 2);
+            personen.add(p);
+            System.out.println("Add Person.");
+        }
+        for (int i = 0; i < anzA; i++)
+        {
+            int rand = randGen.nextInt(n);
+            System.out.println("Setze Meinung bei Person " + rand + ".");
+            personen.get(rand).setMeinung();
+        }
+    }
+
+    /**
+     * Entfernt eine Person aus der Liste der Personen.
+     * @param p zu entfernende Person
+     */
+    void entfernePerson (Person p)
+    {
+        personen.remove(p);
+    }
+
+    /**
+     * Simuliert einen Tag.
+     * Lässt alle Personen in der Klasse eine Begegnung durchführen, mit der
+     * Wahrscheinlichkeit pT.
+     * @param pT Wahrschienlichkeit in Prozent (int 0... 100)
+     */
+    void simTagAbhaengigeMeinung(int pT)
+    {
+        int index = 0;
+        for (Person person : personen)
+        {
+            for (int i = index + 1; i < personen.size(); i++)
+            {
+                int randomNum = randGen.nextInt(101);
+                if (randomNum <= pT) person.abhaengigeMeinung(personen.get(i));
+            }
+            index++;
+        }
+    }
+
+    private void erhöheAnzMeinungen(int n)
+    {
+        this.anzMeinungA = anzMeinungA + n;
+    }
+
+    double meinungsVerteilung ()
+    {
+        anzMeinungA = 0;
+        personen.forEach(person -> {
+            if (person.getAnsichtA())
+            {
+                erhöheAnzMeinungen(1);
+            }
+        });
+        return personen.size()/(double)anzMeinungA;
+    }
+
+    String ausgabeMeinungsverteilung()
+    {
+        String ausgabe = "";
+        for (Person p: personen)
+        {
+            if (p.getAnsichtA())
+            {
+                ausgabe = ausgabe + "|";
+            } else {
+                ausgabe = ausgabe + "-";
+            }
+        }
+        return ausgabe;
+    }
+
+    int getAnzMeinungA()
+    {
+        return anzMeinungA;
+    }
 }
