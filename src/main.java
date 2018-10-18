4de302f994f2170265c531f4f5e88ccd62f819da
A-Petersen <a.petersen@stud.fh-luebeck.de>
2018-10-18 09:25:11 +0200

    Visualisierung angefangen mit .csv etc...

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class main {
     public static void main(String [ ] args) throws IOException {
 
-        Untersuchung u = new Untersuchung(220, 50, 3, 0.8, 2, 10);
+        Untersuchung u = new Untersuchung(300, 50, 3, 0.9, 2, 10);
         u.start();
 
     }
